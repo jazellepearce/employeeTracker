71fcf071e28a2df6a52598acaf692d8383e03bfd
Jazelle Pearce <pjazelle@yahoo.com>
2022-08-07 18:36:36 -0500

    update

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'employees'
+    database: 'employees',
+    password: 'password'
 });
 
 
--- seeds.js
+++ seeds.js
@@ -1,5 +1,3 @@
-
-
 INSERT INTO department (department_name)
 VALUES
 ('LEGAL'),
